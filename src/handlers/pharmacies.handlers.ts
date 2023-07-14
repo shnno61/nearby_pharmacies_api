de490b749fb8d529dce577e094ad1fcbe6580eeb
Ahmed <ahmedelshnawy91@gmail.com>
2023-07-14 15:24:52 +0300

    lung to long

--- src/handlers/pharmacies.handlers.ts
+++ src/handlers/pharmacies.handlers.ts
@@ -4,13 +4,13 @@ const pharmacy1= new Pharmacies();
 
 export const find =  async (req: Request, res: Response) => {
     const lat= req.params.lat as unknown as number;
-    const lung= req.params.lung as unknown as number;
+    const long= req.params.long as unknown as number;
     const name= req.params.name as string;
     const number= req.params.number as unknown as number;
     const radius= req.params.radius as unknown as number;
-    if(typeof lat === "number" && typeof lung === "number" && typeof name === "string" && typeof number === "number"&& typeof radius === "number"){
+    if(typeof lat === "number" && typeof long === "number" && typeof name === "string" && typeof number === "number"&& typeof radius === "number"){
         try {
-            const list = await pharmacy1.searchNearbyPharmacies(lat,lung,radius,name,number);
+            const list = await pharmacy1.searchNearbyPharmacies(lat,long,radius,name,number);
             if (list) {
                 res.status(200).json({
                   status: "found",
