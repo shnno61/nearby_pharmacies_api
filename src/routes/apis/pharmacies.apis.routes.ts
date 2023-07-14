15cc55f6b8e38442a2ed532a45ff7ec762be616f
Ahmed <ahmedelshnawy91@gmail.com>
2023-07-14 14:58:10 +0300

    edited done

--- src/routes/apis/pharmacies.apis.routes.ts
+++ src/routes/apis/pharmacies.apis.routes.ts
@@ -3,5 +3,5 @@ import {
     find
   } from "../../handlers/pharmacies.handlers";
   const get_nearby_pharmacies_routes = Router();
-  get_nearby_pharmacies_routes.route("/:lat/:lung/:name/:number").get(find);
+  get_nearby_pharmacies_routes.route("/:lat/:long/:name/:number").get(find);
   export default get_nearby_pharmacies_routes
--- src/types/results.types.ts
+++ src/types/results.types.ts
@@ -1,4 +1,4 @@
 export type resutls ={
   latitude: string;
-  lungitude:string;
+  longitude:string;
   }
